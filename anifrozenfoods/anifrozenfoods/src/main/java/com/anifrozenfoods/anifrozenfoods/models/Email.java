4b1c2adf44b82b66bd8d60f38eca1597e0e4e57d
Zahiruddin Zahidanishah <65801670+zzahir1978@users.noreply.github.com>
2024-05-22 20:49:54 +0800

    Add files via upload

---
+++ anifrozenfoods/anifrozenfoods/src/main/java/com/anifrozenfoods/anifrozenfoods/models/Email.java
@@ -0,0 +1,42 @@
+/*
+package com.anifrozenfoods.anifrozenfoods.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "phoneBooks")
+public class Email {
+    private String to;
+    private String cc;
+    private String subject;
+    private String content;
+
+    // Getters and Setters
+
+    public String getTo() {
+        return to;
+    }
+    public void setTo(String to) {
+        this.to = to;
+    }
+    public String getCc() {
+        return cc;
+    }
+    public void setCc(String cc) {
+        this.cc = cc;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+}
+*/
---
+++ anifrozenfoods/anifrozenfoods/src/main/java/com/anifrozenfoods/anifrozenfoods/services/EmailService.java
@@ -0,0 +1,13 @@
+/*
+package com.anifrozenfoods.anifrozenfoods.services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anifrozenfoods.anifrozenfoods.models.Email;
+
+public interface EmailService extends JpaRepository<Email, Integer> {
+
+    void sendEmail(jakarta.validation.constraints.Email email);
+
+}
+*/
