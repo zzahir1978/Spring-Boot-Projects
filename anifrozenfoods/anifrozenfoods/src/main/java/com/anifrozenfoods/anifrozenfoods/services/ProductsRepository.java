db23aa1324fdd4474292110b294050d70f051ea5
Zahiruddin Zahidanishah <65801670+zzahir1978@users.noreply.github.com>
2024-05-26 09:46:50 +0800

    Delete anifrozenfoods directory

--- anifrozenfoods/anifrozenfoods/src/main/java/com/anifrozenfoods/anifrozenfoods/services/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anifrozenfoods.anifrozenfoods.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anifrozenfoods.anifrozenfoods.models.Product;
-
-public interface ProductsRepository extends JpaRepository<Product, Integer> {
-
-}
